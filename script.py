6c1142e0dbb9dae63909b48547f245ed0b8706d0
nkerecha <tedmaisiba@gmail.com>
2021-02-19 03:19:13 -0500

    Update

--- script.py
+++ script.py
@@ -14,5 +14,5 @@ def team():
     return render_template('team.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
-    #app.run(debug=True)
+    #app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=True)
