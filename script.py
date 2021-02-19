72b2a2e9fd5236c7e6e4bf069231cef1901f0489
nkerecha <tedmaisiba@gmail.com>
2021-02-19 02:41:59 -0500

    Initial Deployment One

---
+++ script.py
@@ -0,0 +1,18 @@
+# Necessarry imports 
+from flask import Flask,render_template
+from flask import session, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+if __name__ == "__main__":
+    #app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=True)
